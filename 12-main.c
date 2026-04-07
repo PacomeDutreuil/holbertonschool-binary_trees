fe70539504fd7618d5b2e08c220c56d867b683bb
PacomeDutreuil <13007@holbertonstudents.com>
2026-04-07 15:05:35 +0200

    Add 12-main.c

---
+++ 12-main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	binary_tree_t *root;
+	size_t leaves;
+
+	root = binary_tree_node(NULL, 98);
+	if (root == NULL)
+		return (1);
+
+	root->left = binary_tree_node(root, 12);
+	root->right = binary_tree_node(root, 402);
+
+	binary_tree_insert_right(root->left, 54);
+	binary_tree_insert_right(root, 128);
+
+	leaves = binary_tree_leaves(root);
+	printf("Leaves in %d: %lu\n", root->n, leaves);
+
+	leaves = binary_tree_leaves(root->right);
+	printf("Leaves in %d: %lu\n", root->right->n, leaves);
+
+	leaves = binary_tree_leaves(root->left->right);
+	printf("Leaves in %d: %lu\n", root->left->right->n, leaves);
+
+	return (0);
+}
