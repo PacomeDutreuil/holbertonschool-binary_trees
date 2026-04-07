fe9b647df1e6de3bd9643d2272cc7ae318fc58bf
PacomeDutreuil <13007@holbertonstudents.com>
2026-04-07 11:08:22 +0200

    Add bt.h

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,14 @@
 
 #include <stddef.h>
 
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 struct binary_tree_s
 {
 	int n;
@@ -13,8 +21,16 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+/* Task 0 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* Task 1 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/* Task 2 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/* Task 3 */
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif
